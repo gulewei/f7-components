4a426e04ca484124618d54a376ee478680139046
gulewei <gu.lewei@qq.com>
2018-04-26 00:00:22 +0800

    easy scroller

--- src/scroller/easy-scroller.js
+++ src/scroller/easy-scroller.js
@@ -1,9 +1,10 @@
 import Scroller from './scroller'
 
-var EasyScroller = function (content, options) {
+var EasyScroller = function (content, render, options) {
   this.content = content
   this.container = content.parentNode
   this.options = options || {}
+  this.render = render
 
   // create Scroller instance
   var that = this
@@ -20,50 +21,6 @@ var EasyScroller = function (content, options) {
   // reflow for the first time
   this.reflow()
 }
-
-EasyScroller.prototype.render = (function () {
-  var docStyle = document.documentElement.style
-
-  var engine
-  if (window.opera && Object.prototype.toString.call(window.opera) === '[object Opera]') {
-    engine = 'presto'
-  } else if ('MozAppearance' in docStyle) {
-    engine = 'gecko'
-  } else if ('WebkitAppearance' in docStyle) {
-    engine = 'webkit'
-  } else if (typeof navigator.cpuClass === 'string') {
-    engine = 'trident'
-  }
-
-  var vendorPrefix = EasyScroller.vendorPrefix = {
-    trident: 'ms',
-    gecko: 'Moz',
-    webkit: 'Webkit',
-    presto: 'O'
-  }[engine]
-
-  var helperElem = document.createElement('div')
-  var undef
-
-  var perspectiveProperty = vendorPrefix + 'Perspective'
-  var transformProperty = vendorPrefix + 'Transform'
-
-  if (helperElem.style[perspectiveProperty] !== undef) {
-    return function (left, top, zoom) {
-      this.content.style[transformProperty] = 'translate3d(' + (-left) + 'px,' + (-top) + 'px,0) scale(' + zoom + ')'
-    }
-  } else if (helperElem.style[transformProperty] !== undef) {
-    return function (left, top, zoom) {
-      this.content.style[transformProperty] = 'translate(' + (-left) + 'px,' + (-top) + 'px) scale(' + zoom + ')'
-    }
-  } else {
-    return function (left, top, zoom) {
-      this.content.style.marginLeft = left ? (-left / zoom) + 'px' : ''
-      this.content.style.marginTop = top ? (-top / zoom) + 'px' : ''
-      this.content.style.zoom = zoom || ''
-    }
-  }
-})()
 
 EasyScroller.prototype.reflow = function () {
   // set the right scroller dimensions
@@ -163,25 +120,4 @@ EasyScroller.prototype.bindEvents = function () {
   }
 }
 
-// automatically attach an EasyScroller to elements found with the right data attributes
-document.addEventListener('DOMContentLoaded', function () {
-  var elements = document.querySelectorAll('[data-scrollable],[data-zoomable]')
-  var element
-  for (var i = 0; i < elements.length; i++) {
-    element = elements[i]
-    var scrollable = element.attributes.getNamedItem('data-scrollable') ? element.attributes.getNamedItem('data-scrollable').value : null
-    var zoomable = element.attributes.getNamedItem('data-zoomable') ? element.attributes.getNamedItem('data-zoomable').value : ''
-    var zoomOptions = zoomable.split('-')
-    var minZoom = zoomOptions.length > 1 && parseFloat(zoomOptions[0])
-    var maxZoom = zoomOptions.length > 1 && parseFloat(zoomOptions[1])
-
-    // eslint-disable-next-line
-    new EasyScroller(element, {
-      scrollingX: scrollable === 'true' || scrollable === 'x',
-      scrollingY: scrollable === 'true' || scrollable === 'y',
-      zooming: zoomable === 'true' || zoomOptions.length > 1,
-      minZoom: minZoom,
-      maxZoom: maxZoom
-    })
-  };
-}, false)
+export default EasyScroller
