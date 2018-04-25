aee7637e0d6486baf93dd65df44e29e36c163154
gulewei <gu.lewei@qq.com>
2018-04-25 22:43:57 +0800

    gh-pages init

--- src/components/Icon.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line
-import { h } from 'hyperapp'
-import cc from 'classnames'
-
-export const Icon = ({ name, ...r }) => {
-  return (
-    <i {...r} class={cc('icon f7c-icon', 'icon-' + name, r.class)}></i>
-  )
-}
