c8fbdc282c58f635ec0119fbbcd6f9eeda42917f
gulewei <gu.lewei@qq.com>
2018-05-08 14:58:55 +0800

    multiple line list

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -4,6 +4,7 @@ import cc from 'classnames'
 import './style.less'
 
 /**
+ * List block
  * @typedef {Object} ListProps
  * @prop {boolean} [inset=false]
  * @prop {string} [label='']
@@ -18,27 +19,88 @@ const List = (props, children) => {
   } = props
 
   return (
-    <div {...rests} class={cc('list-block', rests.class, { inset })}>
+    <div {...rests} class={cc(rests.class, 'list-block', { inset })}>
       <ul>
-        {children.map(child => <li>{child}</li>)}
+        {renderListChildren(children)}
       </ul>
-      {!!label &&
-        <div class="list-block-label">{label}</div>
+      {label && <div class="list-block-label">{label}</div>}
+    </div>
+  )
+}
+
+const MEDIA_ITEM_CLASS = 'media-item'
+const hasMediaClass = cls => cls.indexOf(MEDIA_ITEM_CLASS) > -1
+function renderListChildren (children) {
+  return children.map(child => (
+    <li
+      key={child.key}
+      class={cc({ [MEDIA_ITEM_CLASS]: hasMediaClass(child.attributes.class) })}
+    >{child}</li>
+  ))
+}
+
+/**
+ * @typedef {Object} ItemProps
+ * @prop {boolean} [isLink=false]
+ * @prop {JSX.Element} [media]
+ * @prop {JSX.Element} title
+ * @prop {JSX.Element} [after]
+ * @prop {JSX.Element} [input]
+ * @prop {JSX.Element} [subTitle]
+ * @prop {JSX.Element} [text]
+ * @param {ItemProps} props
+ */
+const Item = (props) => {
+  const {
+    isLink,
+    media,
+    title,
+    after,
+    input,
+    subTitle,
+    text,
+    ...rests
+  } = props
+
+  const multipleLine = subTitle || text
+  const cls = cc(rests.class, 'item-content', {
+    'item-link': isLink,
+    [MEDIA_ITEM_CLASS]: multipleLine
+  })
+
+  return (
+    <div {...rests} class={cls}>
+      {media && <div class="item-media">{media}</div>}
+      {multipleLine
+        ? <MediaInner {...{ title, after, subTitle, text }} />
+        : <ItemInner {...{ title, after, input }} />
       }
     </div>
   )
 }
 
-const Item = ({ media, title, after, input, isLink, ...r }, children) => {
+// eslint-disable-next-line no-unused-vars
+const ItemInner = ({ title, after, input }) => {
   return (
-    <div class={cc('item-content', { 'item-link': isLink })}>
-      {media && <div class="item-media">{media}</div>}
-      <div class="item-inner">
-        <div class={cc('item-title', { label: !!input })}>{title || children}</div>
-        {input && <div class="item-input">{input}</div>}
+    <div class="item-inner">
+      <div class={cc('item-title', { label: !!input })}>{title}</div>
+      {input && <div class="item-input">{input}</div>}
+      {after && <div class="item-after">{after}</div>}
+    </div>
+  )
+}
+
+// eslint-disable-next-line no-unused-vars
+const MediaInner = ({ title, after, subTitle, text }) => {
+  return (
+    <div class="item-inner">
+      <div class="item-title-row">
+        {title && <div class='item-title'>{title}</div>}
         {after && <div class="item-after">{after}</div>}
       </div>
-    </div >
+      {subTitle && <div class="item-subtitle">{subTitle}</div>}
+      {text && <div class="item-text">{text}</div>}
+    </div>
   )
 }
 
