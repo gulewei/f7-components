28cbee2cd92385dff615cebb3342ddebc5c3fa02
gulewei <gu.lewei@qq.com>
2018-05-08 09:51:08 +0800

    fix list

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -3,19 +3,35 @@ import { h } from 'hyperapp'
 import cc from 'classnames'
 import './style.less'
 
+/**
+ * @typedef {Object} ListProps
+ * @prop {boolean} [inset=false]
+ * @prop {string} [label='']
+ * @param {ListProps} props
+ * @param {JSX.Element[]} children
+ */
 const List = (props, children) => {
+  const {
+    inset,
+    label,
+    ...rests
+  } = props
+
   return (
-    <div {...props} class={cc('list-block', props.class)}>
+    <div {...rests} class={cc('list-block', rests.class, { inset })}>
       <ul>
         {children.map(child => <li>{child}</li>)}
       </ul>
+      {!!label &&
+        <div class="list-block-label">{label}</div>
+      }
     </div>
   )
 }
 
-const Item = ({ media, title, after, input, ...r }, children) => {
+const Item = ({ media, title, after, input, isLink, ...r }, children) => {
   return (
-    <div class={cc('item-content')}>
+    <div class={cc('item-content', { 'item-link': isLink })}>
       {media && <div class="item-media">{media}</div>}
       <div class="item-inner">
         <div class={cc('item-title', { label: !!input })}>{title || children}</div>
