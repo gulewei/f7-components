c1807646a5c4fec54ea035ab0f74fa3971036bb7
gulewei <gu.lewei@qq.com>
2018-05-08 14:59:09 +0800

    add list example

--- demo/list/index.js
+++ demo/list/index.js
@@ -5,9 +5,11 @@ import List, { Item } from '../../src/components/list'
 import ContentBlock from '../../src/components/content-block'
 import Icon from '../../src/components/icon'
 
+const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sagittis tellus ut turpis condimentum, ut dignissim lacus tincidunt. Cras dolor metus, ultrices condimentum sodales sit amet, pharetra sodales eros. Phasellus vel felis tellus. Mauris rutrum ligula nec dapibus feugiat. In vel dui laoreet, commodo augue id, pulvinar lacus.'
+
 app(
   {
-    full: [
+    basic: [
       { title: 'Item Title', after: 'Label', icon: 'f7', isLink: false },
       { title: 'Item Title', after: 'Label', icon: 'f7', isLink: false },
       { title: 'Another Item', after: 'Another Label', icon: 'f7', isLink: false }
@@ -18,6 +20,23 @@ app(
       { title: 'Item Title', after: 'Label', icon: 'f7', isLink: true },
       { title: 'Item Title', after: 'Label', icon: 'f7', isLink: true },
       { title: 'Another Item', after: 'Another Label', icon: 'f7', isLink: true }
+    ],
+
+    multipleLines: [
+      { title: 'Yellow Submarine', after: '$15', subTitle: 'Beatles', text, img: 'http://hhhhold.com/160/d/jpg?1', isLink: true },
+      { title: `Don't Stop Me Now`, after: '$22', subTitle: 'Queen', text, img: 'http://hhhhold.com/160/d/jpg?2', isLink: true },
+      { title: 'Billie Jean', after: '$16', subTitle: 'Michael Jackson', text, img: 'http://hhhhold.com/160/d/jpg?3', isLink: true }
+    ],
+    mails: [
+      { title: 'Facebook', after: '17:14', subTitle: 'New messages from John Doe', text, isLink: true },
+      { title: 'John Doe (via Twitter)', after: '17:14', subTitle: 'John Doe (@_johndoe) mentioned you on Twitter!', text, isLink: true },
+      { title: 'Facebook', after: '17:14', subTitle: 'New messages from John Doe', text, isLink: true },
+      { title: 'John Doe (via Twitter)', after: '17:14', subTitle: 'John Doe (@_johndoe) mentioned you on Twitter!', text, isLink: true }
+    ],
+    simples: [
+      { title: 'Yellow Submarine', subTitle: 'Beatles', isLink: true, icon: 'f7' },
+      { title: `Don't Stop Me Now`, subTitle: 'Queen', isLink: true, icon: 'f7' },
+      { title: 'Billie Jean', subTitle: 'Michael Jackson', isLink: true, icon: 'f7' }
     ]
   },
   {},
@@ -26,7 +45,7 @@ app(
       <Page>
         <ContentBlock title="Full Layout" />
         <List label={state.label}>
-          {state.full.map(({ title, after, icon, isLink }, i) => (
+          {state.basic.map(({ title, after, icon, isLink }, i) => (
             <Item key={i} media={<Icon name={icon} />} {...{ title, after, isLink }} />
           ))}
         </List>
@@ -47,8 +66,36 @@ app(
 
         <ContentBlock title="Inset List Block" />
         <List label={state.label} inset>
-          {state.full.map(({ title, after, icon, isLink }, i) => (
+          {state.basic.map(({ title, after, icon, isLink }, i) => (
             <Item key={i} media={<Icon name={icon} />} {...{ title, after, isLink }} />
+          ))}
+        </List>
+
+        <ContentBlock title="Media List" />
+        <List>
+          {state.multipleLines.map(({ img, ...rest }, i) => (
+            <Item key={i} media={<img src={img} width="80" />} {...rest} />
+          ))}
+        </List>
+
+        <ContentBlock title="Mail App" />
+        <List>
+          {state.mails.map((mail, i) => (
+            <Item key={i} {...mail} />
+          ))}
+        </List>
+
+        <ContentBlock title="Something more simple" />
+        <List>
+          {state.simples.map(({ icon, ...item }, i) => (
+            <Item key={i} media={<Icon name={icon} />} {...item} />
+          ))}
+        </List>
+
+        <ContentBlock title="Inset" />
+        <List inset>
+          {state.simples.map(({ icon, ...item }, i) => (
+            <Item key={i} media={<Icon name={icon} />} {...item} />
           ))}
         </List>
       </Page>
