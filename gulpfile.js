eec85f8b039c026895624b02b20a96b8d6d27de2
gulewei <gu.lewei@qq.com>
2018-07-25 14:58:56 +0800

    es task

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,69 @@
 const gulp = require('gulp')
 const less = require('gulp-less')
 const babel = require('gulp-babel')
-// const path = require('path')
 
+const src = {
+  css: 'components/**/style/index.less',
+  less: 'components/**/*.less',
+  js: 'components/**/*.js',
+  typing: 'components/**/*.d.ts'
+}
+const dest = {
+  lib: 'lib',
+  es: 'es'
+}
+
+let modules = false
+gulp.task('module:true', () => {
+  modules = true
+})
+gulp.task('module:false', () => {
+  modules = false
+})
+
+const getDest = () => modules ? dest.es : dest.lib
+
+// styles
 gulp.task('style:css', () => {
-  return gulp.src('components/**/style/index.less')
+  return gulp.src(src.css)
     .pipe(less())
-    .pipe(gulp.dest('lib'))
+    .pipe(gulp.dest(getDest()))
 })
-
 gulp.task('style:less', () => {
-  return gulp.src([
-    'components/**/*.less'
-  ])
-    .pipe(gulp.dest('lib'))
+  return gulp.src(src.less)
+    .pipe(gulp.dest(getDest()))
 })
-
 gulp.task('style', ['style:css', 'style:less'])
 
+// js
 gulp.task('js:babel', () => {
-  return gulp.src('components/**/*.js')
-    .pipe(babel())
-    .pipe(gulp.dest('lib'))
+  const config = {
+    babelrc: false,
+    presets: [
+      modules ? ['es2015', { 'modules': false }] : 'es2015'
+    ],
+    plugins: [
+      [
+        'transform-react-jsx',
+        {
+          'pragma': 'h'
+        }
+      ],
+      'transform-object-rest-spread',
+      'transform-runtime'
+    ]
+  }
+  return gulp.src(src.js)
+    .pipe(babel(config))
+    .pipe(gulp.dest(getDest()))
 })
-
 gulp.task('js:typing', () => {
-  return gulp.src('components/**/*.d.ts')
-    .pipe(gulp.dest('lib'))
+  return gulp.src(src.typing)
+    .pipe(gulp.dest(getDest()))
 })
-
 gulp.task('js', ['js:babel', 'js:typing'])
+
+// build lib
+gulp.task('lib', ['module:false', 'style:css', 'style:less', 'js:babel', 'js:typing'])
+// build es
+gulp.task('es', ['module:true', 'style:css', 'style:less', 'js:babel', 'js:typing'])
