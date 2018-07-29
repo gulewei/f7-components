3342b59257dae86ac90102e8281121761b87eab7
gulewei <gu.lewei@qq.com>
2018-07-29 23:00:11 +0800

    fix babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
-import { rollup: babelConfig } from './scripts/babel-config'
+import { rollup } from './scripts/babel-config'
 
 export default {
   output: {
@@ -19,6 +19,6 @@ export default {
       include: 'node_modules/**'
     }),
     resolve(),
-    babel(babelConfig)
+    babel(rollup)
   ]
 }
