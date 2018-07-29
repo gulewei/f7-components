3508f16d3cbeaa3507b4314c4ab859f9fc0cce31
gulewei <gu.lewei@qq.com>
2018-07-29 23:16:13 +0800

    fix

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,15 @@
-import commonjs from 'rollup-plugin-commonjs'
-import resolve from 'rollup-plugin-node-resolve'
-import babel from 'rollup-plugin-babel'
-import { rollup } from './scripts/babel-config'
+// import commonjs from 'rollup-plugin-commonjs'
+// import resolve from 'rollup-plugin-node-resolve'
+// import babel from 'rollup-plugin-babel'
+// import { rollup } from './scripts/babel-config'
+const commonjs = require('rollup-plugin-commonjs')
+const resolve = require('rollup-plugin-node-resolve')
+const babel = require('rollup-plugin-babel')
+const config = require('./scripts/babel-config').rollup
 
-export default {
+console.log(JSON.stringify(config))
+
+module.exports = {
   output: {
     file: 'dist/f7-components.js',
     format: 'umd',
@@ -19,6 +25,6 @@ export default {
       include: 'node_modules/**'
     }),
     resolve(),
-    babel(rollup)
+    babel(config)
   ]
 }
--- scripts/babel-config.js
+++ scripts/babel-config.js
@@ -9,16 +9,16 @@ function modulePreset (preset, modules) {
 exports.rollup = {
   babelrc: false,
   exclude: 'node_modules/**',
-  presets: modulePreset(rc.presets[0], true).concat(rc.presets.slice(1)),
+  presets: [modulePreset(rc.presets[0], true)].concat(rc.presets.slice(1)),
   plugins: rc.plugins.concat('external-helpers')
 }
 exports.lib = {
   babelrc: false,
-  presets: modulePreset(rc.presets[0], false).concat(rc.presets.slice(1)),
+  presets: [modulePreset(rc.presets[0], false)].concat(rc.presets.slice(1)),
   plugins: rc.plugins.concat('transform-runtime')
 }
 exports.es = {
   babelrc: false,
-  presets: modulePreset(rc.presets[0], true).concat(rc.presets.slice(1)),
+  presets: [modulePreset(rc.presets[0], true)].concat(rc.presets.slice(1)),
   plugins: rc.plugins.concat('transform-runtime')
 }
