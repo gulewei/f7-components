0c2d042dd59876576e932a0bdadad51d9339f95e
gulewei <gu.lewei@qq.com>
2018-08-08 16:27:47 +0800

    fix custom

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,5 +1,11 @@
 export { h, app } from 'hyperapp'
 export { default as classnames } from 'classnames'
+
 export { default as Dialog } from '../components/dialog'
 export { default as Loading } from '../components/loading'
 export { default as Toast } from '../components/toast'
+
+// dependent
+export { default as Transition } from '../components/transition'
+export { default as Overlay } from '../components/overlay'
+export { default as Preloader } from '../components/preloader'
