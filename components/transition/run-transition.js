693c32d288d09075e7ee83b40c09707768488bc3
gulewei <gu.lewei@qq.com>
2018-10-26 16:42:05 +0800

    try to fix enter raf

--- components/transition/run-transition.js
+++ components/transition/run-transition.js
@@ -30,17 +30,20 @@ export function runEnter (node, enterAnimationActive, enterAnimation, onEntered)
     () => {
       node.classList.add(enterAnimation)
 
+      // raf(function () {
+      //   /**
+      //    * bug: add enter-animation-active classname in this frame won't perform transition as expected, but add in next frame will.
+      //    *
+      //    * I don't know exactly, but I'm guessing that is beacuse we run this after node inserted into document,
+      //    * add enter-animation classname may perform an frame immediately,
+      //    * and add enter-animation-active classname here may merge into repaint in this same frame.
+      //    */
+      //   raf(function () {
+      //     node.classList.add(activeClass)
+      //   })
+      // })
       raf(function () {
-        /**
-         * bug: add enter-animation-active classname in this frame won't perform transition as expected, but add in next frame will.
-         *
-         * I don't know exactly, but I'm guessing that is beacuse we run this after node inserted into document,
-         * add enter-animation classname may perform an frame immediately,
-         * and add enter-animation-active classname here may merge into repaint in this same frame.
-         */
-        raf(function () {
-          node.classList.add(activeClass)
-        })
+        node.classList.add(activeClass)
       })
     },
     () => {
